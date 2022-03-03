46c3f7b8a9a0dc4fa2fefa4d84c628ad63d968b0
fredlarkins <freddie.larkins@hotmail.com>
2022-03-03 14:32:54 +0000

    Finishing the bulk of the work for check-bins.py

--- check-bins.py
+++ check-bins.py
@@ -26,8 +26,8 @@ session = requests.session()
 
 # saving the three different endpoints used throughout this workflow as separate variables
 aqs_query_endpoint = 'https://api.uk.alloyapp.io/api/aqs/query'
-item_endpoint = 'https://api.uk.alloyapp.io/api/aqs/item/'
-workflow_endpoint = 'https://api.uk.alloyapp.io/api/workflow/'
+item_endpoint = 'https://api.uk.alloyapp.io/api/item'
+workflow_endpoint = 'https://api.uk.alloyapp.io/api/workflow'
 
 # reading the json files for the payloads in steps 1 and 4
 with open('payloads/payload_step_1.json') as f1, open('payloads/payload_step_4.json') as f2:
@@ -53,6 +53,7 @@ def convert_to_dict(list_of_dictionaries):
 
 
 # ---------- Asking the user for their postcode ---------- #
+
 postcode = input('\nEnter your postcode > ').lower().strip()
 
 # cleaning the user's input - making sure there is a space in the correct place (as the alloy app query service requires)
@@ -120,12 +121,13 @@ Therefore, we'll use a list of letter that the user can type into the terminal.
 We will have to deal with the situation where the list of addresses exceeds 26.
 '''
 
+print('\nSelect your address from the list below:')
+
 # letter of the alphabet
 letters = list(ascii_lowercase)
 
 # our first conditional: list of addresses is < letters of alphabet
-if len(address_data) < len(letters):
-    print('\nSelect your address from the list below:')
+if len(address_data) <= len(letters):
     
     selection_options = dict(zip(letters, address_data))
 
@@ -134,16 +136,112 @@ if len(address_data) < len(letters):
         # formatting the output in fancy letters
         print(f'{Fore.RED}({letter}){Fore.RESET} {option["attributes"]["attributes_itemsTitle"]}')
     
-    user_selection = input(f'\nType the letter - {Fore.RED}a{Fore.RESET}, {Fore.RED}b{Fore.RESET}, etc - next to your address and hit ENTER > ')
+    user_selection = input(f'\nType the letter - {Fore.RED}a{Fore.RESET}, {Fore.RED}b{Fore.RESET}, etc - next to your address and hit ENTER > ').lower().strip()
     address = selection_options[user_selection]
 
 
+# otherwise, the list of addresses is longer than the alphabet
+# we'll have to use numbers instead - not ideal (and perhaps I should use some sort of 'pagination')
+# but for now, it'll work
+else:
+    selection_options = dict(zip(range(1, len(address_data)), address_data))
+    for number, option in selection_options.items():
+        print(f'{Fore.RED}({number}){Fore.RESET} {option["attributes"]["attributes_itemsTitle"]}')
+    user_selection = int(input(f'\nType the number - {Fore.RED}1{Fore.RESET}, {Fore.RED}2{Fore.RESET}, etc - next to your address and hit ENTER > ').strip())
+    address = selection_options[user_selection]
+
+
+# writing the user's address details to a json file
+with open('address_details.json', 'w') as f:
+    json.dump(address, f)
+
+# confirmation message to use
+print(f'\nThank you! Checking bin collection dates for {Fore.GREEN}{address["attributes"]["attributes_itemsTitle"]}{Fore.RESET}...')
 
 
 
-# # writing the user's address details to a json file
-# with open('address_details.json', 'w') as f:
-#     json.dump(address, f)
+# ---------- Step 2: Using the idemId to check the waste collection services available for that property ---------- #
 
-# # confirmation message to use
-# print(f'\nThank you! Checking bin collection dates for {Fore.GREEN}{address["attributes"]["attributes_itemsTitle"]}...')
+# the alphanumeric identifier for the address
+itemId = address['itemId']
+
+r = session.get(
+    url = f'{item_endpoint}/{itemId}',
+    params = {
+        'token': '67ad2108-dd2b-407a-849a-411a15adf0b1'
+    },
+)
+
+available_service_data = r.json()
+
+# flattening out the list of dicts that are returned in the attributes field again
+available_service_data['item']['attributes'] = convert_to_dict(available_service_data['item']['attributes'])
+
+# properties without waste collection (i.e. flats with communal bins) won't have 'attributes_wasteContainersAssignableWasteContainers' in the dict keys
+if not 'attributes_wasteContainersAssignableWasteContainers' in available_service_data['item']['attributes'].keys():
+    print(f'\nNo waste collection days found for {Fore.GREEN}{address["attributes"]["attributes_itemsTitle"]}{Fore.RESET}. As per the council\'s website: "If you live in an estate or block of flats over 8 properties it is likely you have a communal bin / receive a communal collection."')
+    sys.exit()
+
+
+# retrieving the alphanumeric codes for the available waste services for the given address
+# storing them as keys in a dict (we'll append to the dict in the next section)
+
+available_services = {}
+for service in available_service_data['item']['attributes']['attributes_wasteContainersAssignableWasteContainers']:
+    available_services[service] = {}
+
+
+
+# ---------- Steps 3, 4, 5 ---------- #
+# 3: Mapping each of the attributes_wasteContainersAssignableWasteContainers values to an actual service...
+# 4: Retreiving the endpoints to get the schedules for each waste service
+# 5: Retrieving the timetables for each waste service
+
+
+with open('payloads/payload_step_4.json') as f:
+    payload = json.load(f)
+
+
+for service in available_services.keys():
+
+    # 3:
+    r = session.get(
+        url = f'{item_endpoint}/{service}',
+        params = {
+            'token': '67ad2108-dd2b-407a-849a-411a15adf0b1'
+        }
+    )
+
+    data = r.json()
+
+    # retrieving the 'human-readable' description for the waste service
+    available_services[service]['name'] = convert_to_dict(data['item']['attributes'])['attributes_itemsSubtitle']
+
+    # 4:
+    payload['aqs']['children'][0]['children'][1]['properties']['value'][0] = service
+    p = session.post(
+        url = aqs_query_endpoint,
+        params = {
+            'token': '67ad2108-dd2b-407a-849a-411a15adf0b1'
+        },
+        json = payload
+    )
+
+    workflow_data = p.json()['results'][0]
+    
+    # getting the workflow endpoint from the data
+    available_services[service]['workflowEndpoint'] = convert_to_dict(workflow_data['attributes'])['attributes_scheduleCodeWorkflowID_5f8dbfdce27d98006789b4ec']
+
+    # 5:
+    q = session.get(
+        url = f'{workflow_endpoint}/{available_services[service]["workflowEndpoint"]}',
+        params = {
+            'token': '67ad2108-dd2b-407a-849a-411a15adf0b1'
+        },
+    )
+
+    timetable_data = q.json()['workflow']['workflow']['trigger']['dates']
+
+    available_services[service]['timetable'] = timetable_data
+
+print(available_services)
